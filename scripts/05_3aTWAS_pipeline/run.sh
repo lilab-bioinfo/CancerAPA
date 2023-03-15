8dc2631f8a695fe6f49aced42737c1a0b5f8993d
hchenBioinfo <108267887+hchenBioinfo@users.noreply.github.com>
2023-03-15 16:13:36 +0800

    Update run.sh

--- scripts/05_3aTWAS_pipeline/run.sh
+++ scripts/05_3aTWAS_pipeline/run.sh
@@ -1,10 +1,34 @@
 main(){ 
-  cal_weights
+  compute_weights
+  generate_summary
   run_3aTWAS_analysis
   conditional_analysis
 }
 
-cal_weights(){
+compute_weights(){
+for tiss in `ls tissue.list`
+  do
+    WORK_DIR=`pwd`
+    INPUT_DIR=$WORK_DIR/input
+    OUTPUT_DIR=$WORK_DIR/output
+    PRE="${tiss}"
+    
+    mkdir -p $WORK_DIR/src/slurm/${tiss}
+    
+    PRE_GECP="${PRE}_combined_All_PDUIs_clean.txt"
+    BATCH_START=1
+    BATCH_END=`less ${INPUT_DIR}/APA_matrix/$PRE_GERP}|wc -l`
+    NR="${BATCH_START}_${BATCH_END}"
+    
+    
+  
+  
+done
+}
+
+generate_summary(){
+
+
 
 
 
