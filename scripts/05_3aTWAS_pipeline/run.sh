f831d776f312b7b4aa4eec42e3a047fc9ff927a3
hchenBioinfo <108267887+hchenBioinfo@users.noreply.github.com>
2023-03-15 16:00:37 +0800

    Update run.sh

--- scripts/05_3aTWAS_pipeline/run.sh
+++ scripts/05_3aTWAS_pipeline/run.sh
@@ -1,1 +1,29 @@
+main(){ 
+  cal_weights
+  run_3aTWAS_analysis
+  conditional_analysis
+}
 
+cal_weights(){
+
+
+
+}
+
+run_3aTWAS_analysis(){
+
+
+
+
+
+}
+
+contidional_analysis(){
+
+
+
+
+}
+
+
+main
