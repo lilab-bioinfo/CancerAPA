2db5fab1d02b7c8a6b3fcd5a0ec6b57933d59bbe
hchenBioinfo <108267887+hchenBioinfo@users.noreply.github.com>
2023-03-16 10:35:31 +0800

    Update run.sh

--- scripts/01_plink_clump_and_fine_mapping/run.sh
+++ scripts/01_plink_clump_and_fine_mapping/run.sh
@@ -25,11 +25,11 @@ run_finemapping(){
         if [[ ! -f GLGC_"$name"_result.txt ]]
         then
         cp $file $CUR_DIR/output/CAUSALdb/input
-        mv "$name".txt $CUR_DIR/output/CAUSALdb/input/GLGC_${name}_result.txt  ### unity the name of GLGC_CG0XXX_result.txt
+        mv "$name".txt $CUR_DIR/CAUSALdb/input/GLGC_${name}_result.txt  ### unity the name of GLGC_CG0XXX_result.txt
         fi
 done
   cd $CUR_DIR/output/CAUSALdb
-  python $CUR_DIR/output/CAUSALdb/run_fine_mapping.py -p $CUR_DIR/bin/FinnGen_r5_numberfile.txt >run_fine_mapping.log 2>run_fine_mapping.err &     
+  python $CUR_DIR/CAUSALdb/run_fine_mapping.py -p $CUR_DIR/CAUSALdb/fine_mapping_input.txt >run_fine_mapping.log 2>run_fine_mapping.err &     
 }
 
 
